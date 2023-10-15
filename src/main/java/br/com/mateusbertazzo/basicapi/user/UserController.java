a2944316579d594a28a5e514aef5171706b11d51
Mateus Bertazzo <mateusbataglin52@gmail.com>
2023-10-15 13:41:35 -0300

    implementa encriptação na rota de criação de usuario

--- src/main/java/br/com/mateusbertazzo/basicapi/user/UserController.java
+++ src/main/java/br/com/mateusbertazzo/basicapi/user/UserController.java
@@ -1,4 +1,5 @@
 package br.com.mateusbertazzo.basicapi.user;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -6,6 +7,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 
 @RestController
 @RequestMapping("/users")
@@ -22,6 +26,10 @@ public class UserController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User al ready exists");
         }
 
+        var hashPassword = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+        
+        userModel.setPassword(hashPassword);
+        
         var createdUser = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
     }
